de19b55566b66ee69e5df326f10ace04f96f79c9
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2024-04-29 15:12:33 +0900

    update more dataFeed contract

--- packages/client/src/constant/token.ts
+++ packages/client/src/constant/token.ts
@@ -14,5 +14,17 @@ export const TOKEN_SUPPORTED: Record<any, any> = {
       dataFeed: '0xaC3E04999aEfE44D508cB3f9B972b0Ecd07c1efb',
       dataFeedName: 'LINK / USD',
     },
+    '0xFa94dA175bE505B915187EdC8aE2f62F4Ccbf848': {
+      name: 'DAI',
+      image: 'https://s2.coinmarketcap.com/static/img/coins/128x128/4943.png',
+      dataFeed: '0x9388954B816B2030B003c81A779316394b3f3f11',
+      dataFeedName: 'DAI / USD',
+    },
+    '0x1eeb32A00Bd1746c9389F2E7B5cb7c8262Bf70Ce': {
+      name: 'WBTC',
+      image: 'https://s2.coinmarketcap.com/static/img/coins/128x128/1.png',
+      dataFeed: '0x87dce67002e66C17BC0d723Fe20D736b80CAaFda',
+      dataFeedName: 'WBTC / USD',
+    },
   },
 }
